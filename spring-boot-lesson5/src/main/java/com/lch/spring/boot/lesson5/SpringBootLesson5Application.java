7b99bb7ad1a1978409f4fba063caac2d6f49aaa2
lichanghong <lichanghong@xdf.cn>
2018-08-21 13:25:46 +0800

    第五节

---
+++ spring-boot-lesson5/src/main/java/com/lch/spring/boot/lesson5/SpringBootLesson5Application.java
@@ -0,0 +1,12 @@
+package com.lch.spring.boot.lesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLesson5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLesson5Application.class, args);
+	}
+}
