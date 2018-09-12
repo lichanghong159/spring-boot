09df69fec9b37c55aedfa06c442d5195eb94bf55
lichanghong <lichanghong@xdf.cn>
2018-09-12 09:33:54 +0800

    redisDemo

---
+++ spring-boot-redis/src/main/java/com/lch/spring/boot/springbootredis/SpringBootRedisApplication.java
@@ -0,0 +1,12 @@
+package com.lch.spring.boot.springbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisApplication.class, args);
+	}
+}
