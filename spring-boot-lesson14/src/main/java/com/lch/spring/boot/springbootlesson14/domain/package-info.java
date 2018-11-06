894bd8d064cc2562cc80d17b29f3103d1e96e4e6
lichanghong <lichanghong@xfd.cn>
2018-11-06 22:47:06 +0800

    webservice

---
+++ spring-boot-lesson14/src/main/java/com/lch/spring/boot/springbootlesson14/domain/package-info.java
@@ -0,0 +1,9 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.8-b130911.1802 生成的
+// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2018.11.06 时间 10:33:56 PM CST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://segmentfault.com/schemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.lch.spring.boot.springbootlesson14.domain;
