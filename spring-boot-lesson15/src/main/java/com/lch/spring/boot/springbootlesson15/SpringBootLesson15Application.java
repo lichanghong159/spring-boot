abbb5a0aacb65942ea0114f2cdffb4d419a199db
lichanghong <lichanghong@xfd.cn>
2018-11-26 21:57:08 +0800

    spring security

---
+++ spring-boot-lesson15/src/main/java/com/lch/spring/boot/springbootlesson15/SpringBootLesson15Application.java
@@ -0,0 +1,12 @@
+package com.lch.spring.boot.springbootlesson15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLesson15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLesson15Application.class, args);
+	}
+}
